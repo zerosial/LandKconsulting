8a5affd25eff15145831734e38809b1976c1f4d1
Sunghum Paik <zerosial@gmail.com>
2023-05-21 23:15:52 +0900

    feat: constants로 분리

---
+++ src/lib/components/Constants.ts
@@ -0,0 +1,17 @@
+export const CAROUSEL_IMAGES = [
+  {
+    title: '샘플 1번',
+    text: '샘플 1번',
+    image: '../../../public/picture/cielo1.jpg',
+  },
+  {
+    title: '샘플 2번',
+    text: '샘플 2번',
+    image: '../../../public/picture/cielo2.jpg',
+  },
+  {
+    title: '샘플 3번',
+    text: '샘플 3번',
+    image: '../../../public/picture/orchid1.jpg',
+  },
+];
--- src/lib/components/carousel/carousel.tsx
+++ src/lib/components/carousel/carousel.tsx
@@ -13,6 +13,8 @@ import React from 'react';
 import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
 import Slider from 'react-slick';
 
+import { CAROUSEL_IMAGES } from '../Constants';
+
 const settings = {
   dots: true,
   arrows: false,
@@ -29,26 +31,6 @@ export default function CaptionCarousel() {
   const [slider, setSlider] = React.useState<Slider | null>(null);
   const top = useBreakpointValue({ base: '90%', md: '50%' });
   const side = useBreakpointValue({ base: '30%', md: '40px' });
-  const cards = [
-    {
-      title: '샘플 1번',
-      text: '샘플 1번',
-      image:
-        'https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-    },
-    {
-      title: '샘플 2번',
-      text: '샘플 2번',
-      image:
-        'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80',
-    },
-    {
-      title: '샘플 3번',
-      text: '샘플 3번',
-      image:
-        'https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-    },
-  ];
 
   return (
     <Box position="relative" height="600px" width="full" overflow="hidden">
@@ -87,7 +69,7 @@ export default function CaptionCarousel() {
         <BiRightArrowAlt size="40px" />
       </IconButton>
       <Slider {...settings} ref={(slider) => setSlider(slider)}>
-        {cards.map((card, index) => (
+        {CAROUSEL_IMAGES.map((card, index) => (
           <Box
             key={index}
             height="6xl"
