c2a37a2967eb722e4367cff696046007b9a89463
Sunghum Paik <zerosial@gmail.com>
2023-06-24 01:05:41 +0900

    feat: partners 수정

--- src/lib/pages/partners/index.tsx
+++ src/lib/pages/partners/index.tsx
@@ -14,21 +14,21 @@ import { NextSeo } from 'next-seo';
 interface CardProps {
   heading: string;
   description: string;
+  subDescription: string;
   image: string;
 }
 
-const Card = ({ heading, description, image }: CardProps) => {
+const Card = ({ heading, description, subDescription, image }: CardProps) => {
   return (
     <Box
       maxW={{ base: 'full', md: '275px' }}
-      w="full"
       borderWidth="1px"
       borderRadius="lg"
       overflow="hidden"
       p={5}
     >
       <NextSeo title="Partners" />
-      <Stack align="start" spacing={2}>
+      <Stack spacing={2}>
         <Flex
           w={60}
           h={32}
@@ -40,10 +40,13 @@ const Card = ({ heading, description, image }: CardProps) => {
         >
           <Image src={image} />
         </Flex>
-        <Box pt={4}>
+        <Box pt={4} pl={4}>
           <Heading size="md">{heading}</Heading>
           <Text mt={2} fontSize="sm">
             {description}
+          </Text>
+          <Text mt={2} fontSize="sm" align="center" fontWeight={600}>
+            {subDescription}
           </Text>
         </Box>
       </Stack>
@@ -54,13 +57,14 @@ const Card = ({ heading, description, image }: CardProps) => {
 const Partners = () => {
   return (
     <Box p={4} pt={8}>
-      <Stack spacing={4} as={Container} maxW="5xl" textAlign="center">
+      <Stack spacing={4} as={Container} maxW="5xl">
         <SlideFade in delay={0.5}>
           <Heading
             fontSize={{ base: '2xl', sm: '4xl' }}
             fontWeight="bold"
             fontFamily="mono"
             color={useColorModeValue('purple.400', 'purple.600')}
+            textAlign="center"
           >
             Partners
           </Heading>
@@ -71,9 +75,15 @@ const Partners = () => {
           >
             L&K는 자사 호텔 건축/설계/운영까지 준비과정에서 많은 파트너사와
             협업을 하였습니다. 이 과정에서 선별된 된 파트너사와는 현재까지도
-            업무를 교류하면 함께 파트너 관계를 유지하고 있습니다. 저희의
-            파트너는 클라이언트에게 드리는 한가지 옵션입니다. 충분히 검토를
-            하시고 함께 소통하여 최적화된 파트너를 찾는 것이 핵심입니다.
+            업무를 교류하며 함께 파트너 관계를 유지하고 있습니다.
+          </Text>
+          <Text
+            pt={8}
+            color={useColorModeValue('gray.600', 'gray.200')}
+            fontSize={{ base: 'md', sm: 'lg' }}
+          >
+            저희의 파트너는 클라이언트에게 드리는 한가지 옵션입니다. 충분히
+            검토를 하시고 함께 소통하여 최적화된 파트너를 찾는 것이 핵심입니다.
           </Text>
         </SlideFade>
       </Stack>
@@ -87,6 +97,7 @@ const Partners = () => {
                 '/picture/Lecor.png'
               )}
               description="브랜드 비지니스 전개의 최첨단"
+              subDescription="Branding Company"
             />
             <Card
               heading="Flot"
@@ -95,6 +106,7 @@ const Partners = () => {
                 '/picture/Flot.png'
               )}
               description="호텔에 최적화된 디자인"
+              subDescription="Design Company"
             />
           </Flex>
         </SlideFade>
