490fe7a33a3af21c65bc5b29e34502215cce7d50
Sunghum Paik <zerosial@gmail.com>
2023-07-15 01:49:04 +0900

    refactor: 상수 분리

--- src/lib/pages/partners/index.tsx
+++ src/lib/pages/partners/index.tsx
@@ -9,30 +9,8 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 
+import { PARTNERS_KOREAN_TEXTS } from '~/lib/components/Constants';
 import { Card } from '~/lib/components/partners/Card';
-
-const PARTNERS_KOREAN_TEXTS = {
-  DESCRIPTION:
-    'L&K는 자사 호텔 건축/설계/운영까지 준비과정에서 많은 파트너사와 협업을 하였습니다. 이 과정에서 선별된 된 파트너사와는 현재까지도 업무를 교류하며 함께 파트너 관계를 유지하고 있습니다.',
-  OPTION_DESCRIPTION:
-    '저희의 파트너는 클라이언트에게 드리는 한가지 옵션입니다. 충분히 검토를 하시고 함께 소통하여 최적화된 파트너를 찾는 것이 핵심입니다.',
-  CARDS: [
-    {
-      heading: 'Lecor',
-      description: '브랜드 비지니스 전개의 최첨단',
-      subDescription: 'Branding Company',
-      imageBlack: '/picture/Lecor_black.png',
-      image: '/picture/Lecor.png',
-    },
-    {
-      heading: 'Flot',
-      description: '호텔에 최적화된 디자인',
-      subDescription: 'Design Company',
-      imageBlack: '/picture/Flot_black.png',
-      image: '/picture/Flot.png',
-    },
-  ],
-};
 
 const Partners = () => {
   return (
